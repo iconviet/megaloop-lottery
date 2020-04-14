194709ec2d7993ca2a15e795344909b151720b89
duyyudus <duyyudus@duyyudus-desktop.localdomain>
2020-04-14 14:09:45 +0700

    Add governance controls

--- iconlott/iconlott.py
+++ iconlott/iconlott.py
@@ -4,10 +4,30 @@ TAG = 'IconLott'
 
 
 class IconLott(IconScoreBase):
+    _PLAYERS = 'players'
+    _PLAYERS_RECORD = 'players_record'
+    _MONEY_POT = 'money_pot'
+    _LAST_SETTLEMENT_BH = 'last_settlement_bh'
+    _TOP_DEPOSIT = 'top_deposit'
+
+    # Governance variables
+    _POT_LIMIT = 'pot_limit'
+    _COMMISSION = 'commission'
+    _DEPOSIT_SIZE_LIMIT = 'deposit_size_limit'
+    _TREASURY = 'treasury'
+
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
-        self._players = ArrayDB('players', db, value_type=Address)
-        self._players_record = DictDB('players_record', db, value_type=int)
+        self._players = ArrayDB(self._PLAYERS, db, value_type=Address)
+        self._players_record = DictDB(self._PLAYERS_RECORD, db, value_type=int)
+        self._money_pot = VarDB(self._MONEY_POT, db, value_type=int)
+        self._last_settlement_bh = VarDB(self._LAST_SETTLEMENT_BH, db, value_type=int)
+        self._top_deposit = VarDB(self._TOP_DEPOSIT, db, value_type=int)
+
+        self._pot_limit = VarDB(self._POT_LIMIT, db, value_type=int)
+        self._commission = VarDB(self._COMMISSION, db, value_type=int)
+        self._deposit_size_limit = VarDB(self._DEPOSIT_SIZE_LIMIT, db, value_type=int)
+        self._treasury = VarDB(self._TREASURY, db, value_type=Address)
 
     def on_install(self) -> None:
         super().on_install()
@@ -31,10 +51,83 @@ class IconLott(IconScoreBase):
     @external(readonly=True)
     def about(self) -> str:
         message = 'Welcome to ICONLOTT, a product of ICONVIET'
-        Logger.debug(message, TAG)
         return message
 
     @external(readonly=True)
     def ls_players(self) -> dict:
         players_record = {str(i): self._players_record[i] for i in self._players}
         return players_record
+
+    ###############################################################################################
+    # Governance variables
+
+    @external
+    def set_pot_limit(self, pot_limit: int) -> bool:
+        """
+        In `loop` unit, e.g `1000 * 10 ** 18` loops = `1000` ICX
+        """
+        if self.msg.sender == self.owner:
+            self._pot_limit.set(pot_limit)
+            return True
+        else:
+            return False
+
+    @external(readonly=True)
+    def get_pot_limit(self) -> int:
+        """
+        In `loop` unit, e.g `1000 * 10 ** 18` loops = `1000` ICX
+        """
+        return self._pot_limit.get()
+
+    @external
+    def set_commission(self, commission: int) -> bool:
+        """
+        In percentage unit, e.g. `5` %
+        """
+
+        if self.msg.sender == self.owner:
+            self._commission.set(commission)
+            return True
+        else:
+            return False
+
+    @external(readonly=True)
+    def get_commission(self) -> int:
+        """
+        In percentage unit, e.g. `5` %
+        """
+        return self._commission.get()
+
+    @external
+    def set_deposit_size_limit(self, deposit_size_limit: int) -> bool:
+        """
+        In percentage unit, e.g. `150` %
+        """
+
+        if self.msg.sender == self.owner:
+            self._deposit_size_limit.set(deposit_size_limit)
+            return True
+        else:
+            return False
+
+    @external(readonly=True)
+    def get_deposit_size_limit(self) -> int:
+        """
+        In percentage unit, e.g. `150` %
+        """
+        return self._deposit_size_limit.get()
+
+    @external
+    def set_treasury(self, treasury_address: Address) -> bool:
+        if self.msg.sender == self.owner:
+            self._treasury.set(treasury_address)
+            return True
+        else:
+            return False
+
+    @external(readonly=True)
+    def get_treasury(self) -> Address:
+        return self._treasury.get()
+
+    # End of governance variables
+    ###############################################################################################
