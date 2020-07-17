b7acbc90176860ceab581e88c1ff830949b91fff
Tom Nuen <tomnuen@iconviet.io>
2020-07-18 00:21:17 +0700

    remove extra text

--- megaloop/objects/draw.py
+++ megaloop/objects/draw.py
@@ -1,19 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Copyright 2020 ICONVIET
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 # pylint: disable=W0614
 from .json_base import *
 
@@ -29,11 +13,17 @@ class Draw(JsonBase):
         self.block_count = 0
         self.ticket_count = 0
         self.payout_ratio = 0
+        self.seeding_limit = 0
+        self.revenue_limit = 0
         super().__init__(json)
     
     @property
     def total(self):
         return float(self.prize + self.promo)
+
+    @property
+    def revenue(self):
+        return float(self.total - self.payout)
 
     @property
     def payout(self):
