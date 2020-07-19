7664bea9825edc566eaa8ccb909d2bee8daf680d
Tom Nuen <tomnuen@iconviet.io>
2020-07-19 20:32:08 +0700

    minor reordering

--- megaloop/megaloop_read.py
+++ megaloop/megaloop_read.py
@@ -40,43 +40,42 @@ class MegaloopRead(MegaloopBase):
         return None if not last_draw else str(last_draw)
 
     @external(readonly=True)
-    def get_past_draws(self, skip:int=0, take:int=0, desc:bool=False) -> str:
-        return self.__filter_items_from_json_dict(self._draws, skip, take, desc)
-
-    @external(readonly=True)
     def get_players(self, skip:int=0, take:int=0, desc:bool=False) -> str:
-        return self.__filter_items_from_json_dict(self._players, skip, take, desc)
+        return self.__in_range_order(self._players, skip, take, desc)
 
     @external(readonly=True)
-    def get_past_winners(self, skip:int=0, take:int=0, desc:bool=False) -> str:
-        return self.__filter_items_from_json_dict(self._winners, skip, take, desc)
+    def get_tickets(self, skip:int=0, take:int=0, desc:bool=False) -> str:
+        open_draw = self._open_draw
+        def work(ticket:Ticket):
+            ticket.chance = ticket.amount / open_draw.prize
+            return ticket
+        return self.__in_range_order(self._tickets, skip, take, desc, work)
 
     @external(readonly=True)
     def get_sponsors(self, skip:int=0, take:int=0, desc:bool=False) -> str:
-        return self.__filter_items_from_json_dict(self._sponsors, skip, take, desc)
+        return self.__in_range_order(self._sponsors, skip, take, desc)
+
+    @external(readonly=True)
+    def get_past_draws(self, skip:int=0, take:int=0, desc:bool=False) -> str:
+        return self.__in_range_order(self._draws, skip, take, desc)
+
+    def get_past_winners(self, skip:int=0, take:int=0, desc:bool=False) -> str:
+        return self.__in_range_order(self._winners, skip, take, desc)
 
     @external(readonly=True)
     def get_past_tickets(self, draw_number:str, skip:int=0, take:int=0, desc:bool=False) -> str:
         tickets = Tickets(self._db, draw_number)
         return [str(ticket) for ticket in tickets]
 
-    @external(readonly=True)
-    def get_tickets(self, skip:int=0, take:int=0, desc:bool=False) -> str:
-        open_draw = self._open_draw
-        def calculate_chance(ticket:Ticket):
-            ticket.chance = ticket.amount / open_draw.prize
-            return ticket
-        return self.__filter_items_from_json_dict(self._tickets, skip, take, desc, calculate_chance)
-
-    def __filter_items_from_json_dict(self, json_dict:JsonDictDB, skip:int, take:int, desc:bool, work=None) -> list:
+    def __in_range_order(self, json_dict:JsonDictDB, skip:int, take:int, desc:bool, work=None) -> list:
         sign = 1
         if desc:
             sign = -1
             skip -= sign
         take = skip + take
-        def slip(json:JsonBase):
+        def lazy(json:JsonBase):
             return json
-        func = slip if not work else work
+        func = lazy if not work else work
         if take > len(json_dict): take = len(json_dict)
         if take == 0: return [str(i) for i in json_dict]
         return [str(func(json_dict.get(i * sign))) for i in range(skip, take)]
